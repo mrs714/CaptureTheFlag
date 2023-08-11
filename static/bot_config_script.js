30a2be902610b7270d25d08a054efee4470972eb
mrs <miquel.rodriguez.sansaloni@estudiantat.upc.edu>
2023-08-11 10:51:37 +0200

    sliders are doneee

--- static/bot_config_script.js
+++ static/bot_config_script.js
@@ -2,19 +2,29 @@ health_slider = document.getElementById("health_slider");
 shield_slider = document.getElementById("shield_slider");
 attack_slider = document.getElementById("attack_slider");
 
+health_slider.addEventListener('input', first_slider);
+shield_slider.addEventListener('input', second_slider);
+attack_slider.addEventListener('input', third_slider);
+
 sliders = [health_slider, shield_slider, attack_slider];
 
 update_info();
 
 function first_slider() {
-  var points_to_give = 300 - total_value();
+  var points_to_give = 300 - parseInt(total_value());
   if (points_to_give %2 != 0) {
-    shield_slider.value += parseInt(points_to_give/2) + 1;
-    attack_slider.value += parseInt(points_to_give/2);
+    shield_slider.value = parseInt(shield_slider.value) + parseInt(points_to_give/2);
+    attack_slider.value = parseInt(attack_slider.value) + parseInt(points_to_give/2);
+    if (parseInt(shield_slider.value) < parseInt(attack_slider.value)) {
+      shield_slider.value = parseInt(shield_slider.value) + 1;
+    }
+    else {
+      attack_slider.value = parseInt(attack_slider.value) + 1;
+    }
   }
   else {
-    shield_slider.value += parseInt(points_to_give/2);
-    attack_slider.value += parseInt(points_to_give/2);
+    shield_slider.value = parseInt(shield_slider.value) + parseInt(points_to_give/2);
+    attack_slider.value = parseInt(attack_slider.value) + parseInt(points_to_give/2);
   }
   
 
@@ -23,15 +33,31 @@ function first_slider() {
 
 function second_slider() {
 
-  var points_to_give = 300 - total_value();
-  attack_slider.value += parseInt(points_to_give);
+  var points_to_give = 300 - parseInt(total_value());
+  if (parseInt(attack_slider.value) == 50 && points_to_give < 0){
+    shield_slider.value = parseInt(shield_slider.value) + points_to_give;
+  }
+  else if (parseInt(attack_slider.value) == 150 && points_to_give > 0){
+    shield_slider.value = parseInt(shield_slider.value) + points_to_give;
+  }
+  else {
+    attack_slider.value = parseInt(attack_slider.value) + points_to_give;
+  }
 
   update_info();
 }
 
 function third_slider() {
-  var points_to_give = 300 - total_value();
-  shield_slider.value += parseInt(points_to_give);
+  var points_to_give = 300 - parseInt(total_value());
+  if (parseInt(shield_slider.value) == 50 && points_to_give < 0){
+    attack_slider.value = parseInt(attack_slider.value) + points_to_give;
+  }
+  else if (parseInt(shield_slider.value) == 150 && points_to_give > 0){
+    attack_slider.value = parseInt(attack_slider.value) + points_to_give;
+  }
+  else {
+    shield_slider.value = parseInt(shield_slider.value) + points_to_give;
+  }
 
   update_info();
 }
@@ -43,6 +69,15 @@ function total_value() {
   });
   console.log(total_ammount);
   return total_ammount;
+}
+
+function fix_sliders() {
+  if (total_value() > 300) {
+    health_slider.value = 100;
+  }
+  else {
+    health_slider.value = 300 - total_value();
+  }
 }
 
 function update_info() {
