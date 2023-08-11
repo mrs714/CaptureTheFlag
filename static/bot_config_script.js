a561dad91ea508e562ae2c20687531f71360927c
mrs <miquel.rodriguez.sansaloni@estudiantat.upc.edu>
2023-08-11 09:34:36 +0200

    more sliders

--- static/bot_config_script.js
+++ static/bot_config_script.js
@@ -1,6 +1,26 @@
-const sliders = document.querySelectorAll('.form-control-range');
-const totalPointsDisplay = document.getElementById('totalPoints');
-let totalPoints = 300;
+health_slider = document.getElementById("health_slider");
+shield_slider = document.getElementById("shield_slider");
+attack_slider = document.getElementById("attack_slider");
+
+function first_slider() {
+  var points_to_give = 300 - total_value();
+  if (points_to_give %2 != 0) {
+    shield_slider.value += parseInt(points_to_give/2) + 1;
+    attack_slider.value += parseInt(points_to_give/2);
+  }
+  shield_slider.value += parseInt(points_to_give/2);
+  attack_slider.value += parseInt(points_to_give/2);
+}
+
+function second_slider() {
+  var points_to_give = 300 - total_value();
+  attack_slider.value += parseInt(points_to_give);
+}
+
+function third_slider() {
+  var points_to_give = 300 - total_value();
+  shield_slider.value += parseInt(points_to_give);
+}
 
 sliders.forEach(slider => {
   slider.addEventListener('input', () => {
@@ -8,4 +28,10 @@ sliders.forEach(slider => {
     totalPointsDisplay.textContent = `Total Points: ${totalPoints}`;
   });
 });
-</script>
+
+function total_value() {
+  let total_ammount = 0;
+  sliders.forEach(slider => {
+    total_ammount += parseInt(slider.value);
+  });
+}
