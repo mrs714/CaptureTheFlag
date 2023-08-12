3d439cbefa52020485c1ad1c61e6c21a1b9e08fa
mrs <miquel.rodriguez.sansaloni@estudiantat.upc.edu>
2023-08-12 19:39:30 +0200

    Preparing for player_info

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def uploadConfig(id, health, shield, attack):
     db.save_config(id, json.dumps(config))
 
 def downloadConfig(id):
-    # Get the config of a user
+    # Get the config of a user: health, shield, attack
     return db.get_config(id)
 
 def uploadCode(id, code):
@@ -49,7 +49,7 @@ def downloadCode(id):
     return db.get_code(id)
 
 def downloadInfo(id):
-    # Get the info of a user
+    # Get the info of a user: last position, error last game, date of last game
     return db.get_info(id)
 
 # Pages --------------------------------------------------------------------
