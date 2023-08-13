26a871b73226f8fee071b99e6f9cc7a4b549c1b5
HalcoDraco <pablohalcoguepard@gmail.com>
2023-08-13 17:25:49 +0200

    simul

--- simulation/simulation_consts.py
+++ simulation/simulation_consts.py
@@ -6,8 +6,8 @@ BULLET_SPEED = BOT_SPEED * 4 # pixels per frame
 FPS = 20 # frames per second
 DURATION = 10 * FPS # ticks (converts 60 seconds to ticks)
 INTER_SIMULATION_TIME = 300 # seconds (5 minutes)
-SIM_MP4_NAME = "simulation/simulation.mp4"
-SIM_INFO_NAME = "simulation/simulation_info.txt"
+SIM_MP4_NAME = "replays/simulation.mp4"
+SIM_INFO_NAME = "replays/simulation_info.txt"
 
 BLUE = (0, 0, 255)
 BLACK = (0, 0, 0)
