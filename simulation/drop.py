1b306582cff597dedda779adf1b1b03715e598ef
mrs <miquel.rodriguez.sansaloni@estudiantat.upc.edu>
2023-08-18 21:40:02 +0200

    Drop class implemented

---
+++ simulation/drop.py
@@ -0,0 +1,11 @@
+from simulation.entity import Entity
+from simulation.simulation_consts import *
+from simulation.player_context.drop_info import DropInfo
+
+class Drop(Entity):
+    def __init__(self, sim_id, x, y, type):
+        super().__init__(sim_id, x, y)
+        self.__type = type
+  
+    def get_info(self):
+        return DropInfo(self.id(), self.__x__, self.__y__, self.__type)
---
+++ simulation/player_context/drop_info.py
@@ -0,0 +1,6 @@
+class DropInfo:
+    def __init__(self, sim_id, x, y, type):
+        self.id = sim_id
+        self.x = x
+        self.y = y
+        self.type = type
