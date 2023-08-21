d02a4372eb528225da0b1ff513ee510f589dab94
mrs714 <miquel.rodriguez.sansaloni@estudiantat.upc.edu>
2023-08-21 17:07:28 +0200

    Context added to stop and start server files.

--- start_server.sh
+++ start_server.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-cd CompetitionBotUploader
+echo ""
+echo "Looking for updates on the code..."
 git pull
 
+echo ""
+echo "Launching server."
 # Start Flask in a detached screen session
 screen -dmS flask_screen bash -c 'flask run --host=0.0.0.0 --port=80'
 
+echo ""
+echo "Launching simulation."
 # Run the simulation_launch.py script
 screen -dmS simulation_screen bash -c 'python3 simulation_launch.py'
+echo ""
--- stop_server.sh
+++ stop_server.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 # Send the 'quit' command to the screen session named 'my_session_name'
+echo ""
+echo "Stoping server."
 screen -S flask_screen -X quit
 
+echo ""
+echo "Stoping simulation."
 screen -S simulation_screen -X quit
+echo ""
