d3df4a00185ccf21846c50477a83fc6df28b08fa
mrs <miquel.rodriguez.sansaloni@estudiantat.upc.edu>
2023-08-23 16:54:34 +0200

    More instructions for server setup

--- start_server.sh
+++ start_server.sh
@@ -9,33 +9,39 @@ echo "Launching server."
 # Start Flask in a detached screen session
 screen -dmS flask_screen bash -c 'flask run --host=0.0.0.0 --port=80'
 
-# Parse command-line arguments
-while [[ $# -gt 0 ]]; do
-    case $1 in
-        --map-width)
-            MAP_WIDTH="$2"
-            shift 2
-            ;;
-        --map-height)
-            MAP_HEIGHT="$2"
-            shift 2
-            ;;
-        --duration)
-            DURATION="$2"
-            shift 2
-            ;;
-        *)
-            echo "Unknown option: $1"
-            exit 1
-            ;;
-    esac
-done
+# Check if any command-line arguments are provided
+if [[ $# -gt 0 ]]; then
+    # Parse command-line arguments
+    while [[ $# -gt 0 ]]; do
+        case $1 in
+            --map-width)
+                MAP_WIDTH="$2"
+                shift 2
+                ;;
+            --map-height)
+                MAP_HEIGHT="$2"
+                shift 2
+                ;;
+            --duration)
+                DURATION="$2"
+                shift 2
+                ;;
+            *)
+                echo "Unknown option: $1"
+                exit 1
+                ;;
+        esac
+    done
 
-# Call the Python script with the provided arguments
-screen -dms flask_screen bash -c 'python simulation.py --map-width "$MAP_WIDTH" --map-height "$MAP_HEIGHT" --duration "$DURATION"'
+    echo ""
+    echo "Launching simulation."
+    # Call the Python script with the provided arguments
+    screen -dmS flask_screen bash -c 'cpulimit -l 90 -e python simulation.py --map-width "$MAP_WIDTH" --map-height "$MAP_HEIGHT" --duration "$DURATION"'
+    echo ""
+fi
 
 echo ""
 echo "Launching simulation."
 # Run the simulation_launch.py script
-screen -dmS simulation_screen bash -c 'python3 simulation_launch.py'
+screen -dmS simulation_screen bash -c 'cpulimit -l 90 -e python3 simulation_launch.py'
 echo ""
