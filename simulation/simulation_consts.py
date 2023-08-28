209d8c2aa1152e8187536055ae07f003b4da9dd4
Miquel Rodríguez Sansaloni <miquel.rodriguez.sansaloni@estudiantat.upc.edu>
2023-08-28 22:19:01 +0200

    Config changes

--- simulation/simulation_consts.py
+++ simulation/simulation_consts.py
@@ -1,6 +1,6 @@
 import math
 
-MAP_WIDTH, MAP_HEIGHT = 1000, 1000 # pixels, actual playing zone
+MAP_WIDTH, MAP_HEIGHT = 600, 600 # pixels, actual playing zone
 MAP_PADDING = 50 # padding at each side of the map
 BULLET_RADIUS = 4 # pixels
 SUPER_BULLET_RADIUS = int(BULLET_RADIUS * 1.5)
@@ -9,7 +9,7 @@ DROP_RADIUS = 2 * BULLET_RADIUS # pixels
 BOT_SPEED = 3 # pixels per frame
 BULLET_SPEED = BOT_SPEED * 4 # pixels per frame
 FPS = 20 # frames per second
-DURATION = 2 * FPS # In seconds (x FPS = 1 second)
+DURATION = 60 * FPS # In seconds (x FPS = 1 second)
 INTER_SIMULATION_TIME = 60 # seconds
 MAX_FRAMES_ON_RAM = 100000000 / (MAP_HEIGHT * MAP_WIDTH) # Maximum ammount of frames on RAM. e.g.: 350x350pixels = 81 frames stored, 1000*1000pixels = 10 frames stored
 SIM_FOLDER = "replays/"
