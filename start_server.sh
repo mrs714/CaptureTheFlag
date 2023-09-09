03151ed4f9a3b0ae3dc5087638918a01de52eaad
Miquel Rodríguez Sansaloni <miquel.rodriguez.sansaloni@estudiantat.upc.edu>
2023-09-09 21:44:57 +0200

    Check server included in start/stop server

--- start_server.sh
+++ start_server.sh
@@ -45,3 +45,8 @@ echo "Launching simulation."
 # Run the simulation_launch.py script
 screen -dmS simulation_screen bash -c 'python3 simulation_launch.py > simulation.log 2>&1'
 echo ""
+echo "Launching simulation checker."
+# Run the check_server.py script
+screen -dmS check_screen bash -c 'python3 check_server.py'
+echo ""
+
