f1c994eebe5afbd7c6f242f6ec561741adceee20
Miquel Rodríguez Sansaloni <miquel.rodriguez.sansaloni@estudiantat.upc.edu>
2023-09-09 22:27:19 +0200

    Check server now restarts it when it fails

--- server/check_server.py
+++ server/check_server.py
@@ -2,6 +2,17 @@ import subprocess
 import time
 from pushbullet import Pushbullet
 
+
+# Replace with your Pushbullet API key
+pushbullet_api_key = "your_pushbullet_api_key"
+
+# Interval in seconds to check if the session is running
+check_interval = 60  # Change this to your desired interval
+log_path = "simulation.log" # Change this to the path of your log file
+lines_to_read = 50 # Change this to the number of lines you want to read from the log file (last x lines)
+restart_server_on_crash = True # Change this to False if you don't want the server to restart automatically
+restart_script_path = "restart_server.py" # Change this to the path of your restart script
+
 # Function to check if the screen session is running
 def is_screen_session_running(session_name):
     try:
@@ -23,15 +34,6 @@ def send_pushbullet_notification(api_key, title, message):
     push = pb.push_note(title, message)
     print("Pushbullet notification sent successfully.")
 
-# Replace with your Pushbullet API key
-pushbullet_api_key = "your_pushbullet_api_key"
-
-# Interval in seconds to check if the session is running
-check_interval = 60  # Change this to your desired interval
-
-log_path = "simulation.log"
-lines_to_read = 50
-
 while True:
     if is_screen_session_running("simulation_screen"):
         print("The 'simulation_screen' session is running.")
@@ -45,6 +47,13 @@ while True:
             log_contents = f.readlines()[-lines_to_read:] # Get the last 50 lines of the log file
         # Send a Pushbullet notification with the log file contents
         send_pushbullet_notification(pushbullet_api_key, "Session Not Running", "The 'simulation_screen' session is not running." + "\n\n" + log_contents)
+
+        if restart_server_on_crash:
+            try:
+                subprocess.run(["python3", restart_script_path], check=True)
+                print(f"{restart_script_path} executed successfully.")
+            except subprocess.CalledProcessError as e:
+                print(f"Error executing {restart_script_path}: {e}")
     
     # Wait for the specified interval before checking again
     time.sleep(check_interval)
---
+++ server/restart_server.py
@@ -0,0 +1,16 @@
+import subprocess
+
+# Define the paths to the start and stop scripts
+start_script_path = "../start_server.sh"  # Adjust the path as needed
+stop_script_path = "../stop_server.sh"    # Adjust the path as needed
+
+def execute_script(script_path, script_name):
+    try:
+        subprocess.run([script_path], check=True, shell=True)
+        print(f"{script_name} executed successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error executing {script_name}: {e}")
+
+# Restart the server
+execute_script(stop_script_path, "stop_server.sh")
+execute_script(start_script_path, "start_server.sh")
