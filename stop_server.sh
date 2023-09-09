8980cbcba63e4865f89d7948e7df1cefd0f32989
Miquel Rodríguez Sansaloni <miquel.rodriguez.sansaloni@estudiantat.upc.edu>
2023-09-09 22:06:36 +0200

    Revert "Server things have a folder now"
    This reverts commit f357f4606fbc8f6e61de77dbca651dd42978ee06.

--- stop_server.sh
+++ stop_server.sh
@@ -6,12 +6,11 @@ echo "Stoping server."
 screen -S flask_screen -X quit
 
 echo ""
-
 echo "Stoping simulation checker."
 screen -S check_screen -X quit
-
 echo ""
 
+echo ""
 echo "Stoping simulation."
 screen -S simulation_screen -X quit
 echo ""
